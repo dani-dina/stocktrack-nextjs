8d72a7f68cde0793c4bfdec6cccc4dfc17e440f5
Daniel <danielkibret8@gmail.com>
2025-08-07 18:06:44 +0300

    Navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Button from './Button';
 import { FaX } from 'react-icons/fa6';
+import { navLinks } from '../constants';
 
 const Navigation : React.FC = () =>{
   return (
@@ -18,13 +19,32 @@ const Navigation : React.FC = () =>{
           />
         </Link>
         <nav className=''>
-          <div className=''>
-
+          <div className='flex gap-3'>
+            {
+              navLinks.map((items) =>(
+                <Link
+                  key={items.id}
+                  href={items.href}
+                >
+                  {items.label}
+                </Link>
+              ))
+            }
           </div>
           <Button
             children={<FaX />}
+            className='md:hidden'
           />
         </nav>
+        <div className='flex gap-4'>
+          <Button 
+            children='Sign up'
+          />
+          <Button
+            children='Log in'
+            className='py-1 px-2 border-1 border-gray-400 rounded-md'
+          />
+        </div>
       </div>
     </div>
   );
