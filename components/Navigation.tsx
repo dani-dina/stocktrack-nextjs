2f2ecf71a5be7d8dcb170f8307e2c97f0cded855
Daniel <danielkibret8@gmail.com>
2025-08-07 17:55:35 +0300

    Navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,75 +1,33 @@
-'use client'
-
-import { FiMenu, FiX } from 'react-icons/fi';
-import Image from 'next/image';
-import { navLinks } from '../constants';
 import Link from 'next/link';
+import Image from 'next/image';
 import Button from './Button';
-import { useState } from 'react';
-import { set } from 'zod';
+import { FaX } from 'react-icons/fa6';
 
 const Navigation : React.FC = () =>{
-  const [toggle,setToggle] = useState(false);
-
-  const handleMenu = () => {
-    if(toggle){
-      setToggle(!toggle)
-      console.log(!toggle)
-    }else{
-      setToggle(!toggle);
-      console.log(!toggle);
-    }
-  }
-
   return (
-    <div className='w-full'>
-      <div className='w-full my-4 flex items-center justify-around'>
-        {/* Logo section */}
+    <div className='fixed top-0 left-0 w-full'>
+      <div className='flex items-center justify-between mx-15 my-5'>
         <Link
           href={'/'}
         >
           <Image
-          src={'/logo.png'}
-          width={200}
-          height={70}
-          alt='Logo'
-        />
-        </Link>
-        {/* Right side navigations*/}
-        <div className={`${toggle ? 'flex' : 'hidden'} md:flex w-full md:w-3/5 items-center justify-between`}>
-          <nav className='fixed top-5 flex flex-col w-full h-full bg-red-800 md:flex-row gap-4'>
-            {
-              navLinks.map((items) =>(
-                <Link
-                  key={items.id}
-                  href={items.href}
-                  className='font-[poppins]'
-                >
-                  {items.label}
-                </Link>
-              ))
-            }
-          </nav>
-          <div className='hidden md:flex gap-2 m-2'>
-            <Button 
-              className=''
-              children='Sign in'
-            />
-            <Button 
-            className='border-1 border-primary ml-2 py-1 px-4 rounded-md'
-            children={'Log in'}
-            />
-          </div>
-        </div>
-        <div className='md:hidden'>
-          <Button
-            children={toggle ? <FiX width={30}/> : <FiMenu />}
-            onClick={handleMenu}
+            src={'/logo.png'}
+            width={150}
+            height={60}
+            alt='LOGO'
           />
-        </div>
+        </Link>
+        <nav className=''>
+          <div className=''>
+
+          </div>
+          <Button
+            children={<FaX />}
+          />
+        </nav>
       </div>
     </div>
   );
-} 
+}
 
 export default Navigation;
