adddf0fd5281802662992e0f81ae60df71ccac21
Daniel <danielkibret8@gmail.com>
2025-08-12 02:22:33 +0300

    Pricing

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -1,6 +1,10 @@
+'use client'
 import React from "react";
+import { useState } from "react";
+import Button from "./Button";
 
 const Pricing : React.FC = () => {
+  const [ planType,setPlanType ] = useState<'annual' | 'monthly'>('annual');
   return (
     <section className='w-full'>
       <div className='mx-7 md:mx-15'>
@@ -10,6 +14,20 @@ const Pricing : React.FC = () => {
         <p className='text-center my-4'>
           Here’s a JavaScript array with 6 objects for your inventory management system’s <br /> features page, following your requested format:
         </p>
+        <div className='w-full flex items-center justify-center'>
+          <div className='py-1.5 px-4  bg-white/10 backdrop-blur-md border border-white/20  rounded-3xl flex gap-4 mt-2'>
+            <Button 
+              children='Anually'
+              className={`${ planType === 'annual' ? 'bg-white/10 backdrop-blur-md border border-white/40 rounded-3xl px-4 py-2' : '' } `} 
+              onClick={ () => setPlanType('annual') }
+            />
+            <Button
+              children='Monthly'
+              className={`${ planType === 'monthly' ? 'bg-white/10 backdrop-blur-md border border-white/40 rounded-3xl px-4 py-2' : '' } `} 
+              onClick={()=> setPlanType('monthly')}
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
